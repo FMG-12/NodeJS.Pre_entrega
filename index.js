dbe63a0f5c6557dea914fb4b968ef62195d1a14e
FMG-12 <fvmiano@gmail.com>
2026-04-20 23:12:17 -0300

    Primer commit. Metodos listos, uso del destructuring

---
+++ index.js
@@ -0,0 +1,78 @@
+const args = process.argv.slice(2);
+
+//argumentos
+const [method, url, title, price, category] = args;
+const route = url?.split("/")[0];
+const article = url?.split("/")[1];
+
+//console.log(method, url, title, price, category);
+
+class Product {
+    constructor (title, price, category){
+        this.title = title;
+        this.price = price;
+        this.category = category;
+    }
+}
+
+switch(method){
+    case "GET":
+        //si no me consulta por una articulo en especifico le devuelvo todos los productos
+        if (route === "products" && !article){
+            fetch('https://fakestoreapi.com/products', {
+                method: "GET"
+            }
+            )
+            .then(response => response.json())
+            .then(json => console.log(json))
+            .catch(error => console.log(error));
+        }
+        //valido si el usuario me está consultando por un producto específico
+        else if (route === "products" && !isNaN(article)){   //Con la función isNan verifico si el segundo argumento es un número   
+            fetch(`https://fakestoreapi.com/products/${article}`, {
+                method: "GET"
+            }
+            )
+            .then(response => response.json())
+            .then(json => console.log(json))
+            .catch(error => console.log(error));
+        }
+        else {
+            throw new Error("Argumento faltante o no válido.");
+        }
+
+    break;
+
+    case "POST":
+
+            //Primero valido si todos los argumentos que necesito estan completos y son validos, sino informo un error.
+            if(route!= "products" || !title || !price || !category){
+                throw new Error("Argumentos faltantes o no válidos.");
+            }
+            
+            const new_product = new Product(title, price, category);
+
+            fetch("https://fakestoreapi.com/products",{
+                    method: "POST",
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(new_product)
+                }
+            )
+            .then(response => response.json())
+            .then(data => console.log(data));
+    break;
+
+    case "DELETE":
+
+            //Primero valido si todos los argumentos que necesito estan completos y son validos, sino informo un error.
+            if(route!= "products" || isNaN(article)){
+                throw new Error("Argumentos faltantes o no válidos.");
+            }
+            
+            fetch(`https://fakestoreapi.com/products/${article}`,{
+                method: "DELETE"
+            })
+            .then(response => response.json())
+            .then(data => console.log(data));
+    break;
+}
